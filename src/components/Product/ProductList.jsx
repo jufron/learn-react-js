aa0fef5f17f296f3d95ef801619b54997ad8787f
jufrontamoama <jufrontamoama@gmail.com>
2025-03-30 09:17:59 +0000

    useEffect dependencis, how to use and behavior

--- src/components/Product/ProductList.jsx
+++ src/components/Product/ProductList.jsx
@@ -3,37 +3,41 @@ import Product from "./Product";
 
 const ProductList = () => {
   const [ products, setProducts ] = useState([]);
-  const [ loadingContent, setLoadingContent ] = useState(true);
-  const loaded = useRef(false);
+  const [ load, setLoad ] = useState(false);
+  const [ loadingAnimation, setLoadingAnimation ] = useState(false);
+
+  function loadProduct () {
+    setLoad(true);
+    setLoadingAnimation(true);
+  }
 
   function callApi() {
-    console.log('melakukan fetch data');
     setTimeout(() => {
       fetch('/product.json')
         .then(response => response.json())
         .then(data => setProducts(data))
         .then(() => loaded.current = true)
         .catch(() => error => console.log(error))
-        .finally(() => setLoadingContent(false));
-
-    }, 2000);
+        .finally( () => setLoadingAnimation(false))
+    }, 100);
   }
   
   useEffect(() => {
-    if (!loaded.current) {
-      callApi();
-    }
-
+    load && callApi();
     return () => console.log('product list component unmoundted');
   }, []);
 
   return (
     <>
       <h1>Product list</h1>
-      {loadingContent && <p>Loading...</p>}
-      {products.map((product) => (
-        <Product key={product.id} product={product} />
-      ))}
+      {loadingAnimation && <p>loading...</p>}
+      {!load ? (
+        <button onClick={loadProduct}>load product</button>
+      ) : (
+        products.map((product) => (
+          <Product key={product.id} product={product} />
+        ))
+      )}
     </>
   )
 }
