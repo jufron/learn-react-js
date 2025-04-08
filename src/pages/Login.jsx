71a90bfb7b3ff33c3a040d3c4fa57116b738c3d8
jufrontamoama <jufrontamoama@gmail.com>
2025-04-08 14:22:22 +0000

    add footer and add design card for index page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,38 +1,41 @@
 import InputForm from "../components/auth/InputForm";
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
-
 
 export default function Login() {
   return (
-    <div className="h-screen bg-gray-100 flex flex-col">
+    <>
       <Navbar />
-      <div className="flex-grow flex items-center justify-center">
-        <div className="bg-white p-8 rounded-lg shadow-md w-96">
-          <h1 className="text-3xl font-bold text-center mb-6">Login</h1>
-          <form>
-            <InputForm 
-              labelName="Email"
-              inputName="email"
-              id="email"
-              type="email"
-              placeholder="Your email"
-            />
-            <InputForm
-              labelName="Password"
-              inputName="password"
-              id="password"
-              type="password"
-              placeholder="Your password"
-            />
-            
-            <div className="flex items-center justify-center">
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-                Sign In
-              </button>
-            </div>
-          </form>
-        </div>
+      <div className="h-screen bg-gray-100 flex flex-col">
+        <div className="flex-grow flex items-center justify-center">
+          <div className="bg-white p-8 rounded-lg shadow-md w-96">
+            <h1 className="text-3xl font-bold text-center mb-6">Login</h1>
+            <form>
+              <InputForm 
+                labelName="Email"
+                inputName="email"
+                id="email"
+                type="email"
+                placeholder="Your email"
+              />
+              <InputForm
+                labelName="Password"
+                inputName="password"
+                id="password"
+                type="password"
+                placeholder="Your password"
+              />
+              
+              <div className="flex items-center justify-center">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                  Sign In
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>    
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,48 +1,52 @@
 import InputForm from "../components/auth/InputForm";
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 
 export default function Register() {
   return (
-    <div className="min-h-screen bg-gray-100">
+    <>
       <Navbar />
-      <div className="flex items-center justify-center mt-16">
-        <div className="bg-white p-8 rounded-lg shadow-md w-96">
-          <h2 className="text-2xl font-bold text-center mb-6">Register</h2>
-          <form>
-            <InputForm 
-              labelName="Username"
-              inputName="username"
-              id="username"
-              type="text"
-              placeholder="Your Username"
-            />
-            <InputForm 
-              labelName="Email"
-              inputName="email"
-              id="email"
-              type="email"
-              placeholder="Your Email"
-            />
-            <InputForm 
-              labelName="Password"
-              inputName="password"
-              id="password"
-              type="password"
-              placeholder="Your Password"
-            />
-            <InputForm 
-              labelName="Confirm Password"
-              inputName="confirmPassword"
-              id="confirmPassword"
-              type="password"
-              placeholder="Confirm Password"
-            />
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-              Register
-            </button>
-          </form>
+      <div className="min-h-screen bg-gray-100">
+        <div className="flex items-center justify-center mt-16">
+          <div className="bg-white p-8 rounded-lg shadow-md w-96">
+            <h2 className="text-2xl font-bold text-center mb-6">Register</h2>
+            <form>
+              <InputForm 
+                labelName="Username"
+                inputName="username"
+                id="username"
+                type="text"
+                placeholder="Your Username"
+              />
+              <InputForm 
+                labelName="Email"
+                inputName="email"
+                id="email"
+                type="email"
+                placeholder="Your Email"
+              />
+              <InputForm 
+                labelName="Password"
+                inputName="password"
+                id="password"
+                type="password"
+                placeholder="Your Password"
+              />
+              <InputForm 
+                labelName="Confirm Password"
+                inputName="confirmPassword"
+                id="confirmPassword"
+                type="password"
+                placeholder="Confirm Password"
+              />
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                Register
+              </button>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
